e60a52e1cc8ee1708c13402c521bca1a27b569f7
Richard Stacey <staceyrichard1996@gmail.com>
2023-03-24 16:38:59 -0600

    html structure set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+let message = "Gotta Catch them all!"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+    <h1>{message}</h1>
+    <div>
+      <h2>#249 Lugia</h2>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/249.png" alt="pokemon"/>
+      <h3>Types</h3>
+      <ul>
+        <li class="psychic">psychic</li>
+        <li class="flying">flying</li>
+      </ul>
+      <h3>Height: 5.2 M</h3>
+      <h3>Weight: 216 KG</h3>
     </div>
+    </body>
   );
 }
 
