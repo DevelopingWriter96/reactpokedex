2a66d77707699851f27b3d3e64088cb63afb0a4c
Richard Stacey <staceyrichard1996@gmail.com>
2023-03-29 14:53:30 -0600

    Tyep sort and specific Pokemon Functional

--- src/App.js
+++ src/App.js
@@ -1,89 +1,109 @@
 import './App.css';
+import { useState } from 'react';
+
+function App() {
 
 let message = "Gotta Catch them all!"
 
-let pokeList = []
+const [ poke, setPoke ] = useState([])
+
+const [ selectPoke, setSelectPoke ] = useState({})
+
+const [ typeSort, setTypeSort ] = useState([])
+
+const [ types, setTypes ] = useState([])
+
+const [ src, setSrc ] = useState("")
 
 function toggleFavorite() {
   console.log("Toggled")
+}
+
+function sortType(type) {
+  fetch(`https://pokeapi.co/api/v2/type/${type}/`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setTypeSort(data.pokemon);
+      setPoke([]);
+    })
+}
+
+function PokeData() {
+  return (
+    <>
+    <h2>#{selectPoke.id} {selectPoke.name}</h2>
+      <img src={src}  alt="pokemon"/>
+      <h3>Types</h3>
+      <ul>
+        {pokeTypes}
+      </ul>
+      <h3>Height: {selectPoke.height / 10} M</h3>
+      <h3>Weight: {selectPoke.weight / 10} KG</h3>
+    </>
+  )
 }
 
 async function getPokemon() {
   const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
     const data = await res.json()
 
-    let poke = data.results
+    setPoke(data.results)
 
     console.log(poke)
 
-    pokeList = poke.map((pokemon) =>
-      <li key={pokemon.name} onClick={selectPokemon(pokemon.url)}>{pokemon.name}</li>
-    )
 }
 
 async function selectPokemon(select) {
     const res = await fetch(select)
     const data = await res.json()
 
-    let selectedPoke = data;
+    setSelectPoke(data);
 
-    console.log(selectedPoke)
+    console.log(selectPoke)
 
-    let src = data.sprites.front_default
+    setSrc(data.sprites.front_default)
 
     console.log(src)
 
-    let types = data.types
+    setTypes(data.types);
 
     console.log(types);
+
    }
 
 function sortFavorites() {
   console.log("Favorites go here")
 }
 
-function App() {
-  return (
+const pokeTypes = types.map((type) => {
+  return <li className={type.type.name} key={type.type.name} onClick={() => {
+    sortType(type.type.name)}}>{type.type.name}</li>
+ })
+
+const pokeList = poke.map((pokemon) => {
+  return <li onClick={() => {
+    selectPokemon(pokemon.url)
+  }} key={pokemon.name}>{pokemon.name}</li>;
+})
+
+const typeSortList = typeSort.map((type) => {
+  return <li onClick={() => {
+    selectPokemon(type.pokemon.url)
+  }} key={type.pokemon.name}>{type.pokemon.name}</li>;
+})
+
+
+return (
     <>
     <h1>{message}</h1>
     <div>
-      <h2>#249 Lugia</h2>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/249.png" alt="pokemon"/>
-      <h3>Types</h3>
-      <ul>
-        <li className="psychic">psychic</li>
-        <li className="flying">flying</li>
-      </ul>
-      <h3>Height: 5.2 M</h3>
-      <h3>Weight: 216 KG</h3>
+      <PokeData />
       <button onClick={toggleFavorite}>Toggle Favorite</button>
       <button onClick={sortFavorites}>Sort Favorites</button>
       <button onClick={getPokemon}>Get Pokemon</button>
-      <select><option>Choose One</option>
-    <option value="normal">Normal</option>
-    <option value="fire">Fire</option>
-    <option value="water">Water</option>
-    <option value="grass">Grass</option>
-    <option value="electric">Electric</option>
-    <option value="ice">Ice</option>
-    <option value="fighting">Fighting</option>
-    <option value="poison">Poison</option>
-    <option value="ground">Ground</option>
-    <option value="flying">Flying</option>
-    <option value="psychic">Psychic</option>
-    <option value="bug">Bug</option>
-    <option value="rock">Rock</option>
-    <option value="ghost">Ghost</option>
-    <option value="dark">Dark</option>
-    <option value="dragon">Dragon</option>
-    <option value="steel">Steel</option>
-    <option value="fairy">Fairy</option></select>
-    <ul id="pokemon">
-      {pokeList}
-    </ul>
-    <ul id="type">
-
-    </ul>
+    <ul id="pokemon">{pokeList}</ul>
+    <ul>{typeSortList}</ul>
     </div>
     </>
   );
